b0c24f02007c10563811356157ac8f5fb7009488
ishmamk1 <ishmamkhan1010@gmail.com>
2025-10-13 23:08:49 -0400

    calc api app

---
+++ go-practice/http-stuff/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/item/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		w.Write([]byte("recieved request for item: " + id))
+	})
+
+	server := http.Server{
+		Addr: ":8080",
+		Handler: router,
+	}
+	log.Println("Starting server on port : 8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
